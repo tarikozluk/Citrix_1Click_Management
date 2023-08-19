639727ef658818fd02eaa225add8accc93198b6d
tarik <47662299+tarikozluk@users.noreply.github.com>
2023-08-19 14:41:53 +0300

    basic auth added for root and guest password

--- main.py
+++ main.py
@@ -6,10 +6,29 @@ import json
 import time
 from flask_cors import CORS
 import dns_implementer
+from flask_httpauth import HTTPBasicAuth
 
 load_dotenv()
 app = Flask(__name__)
+auth = HTTPBasicAuth()
 cors = CORS(app)
+
+VALID_CREDENTIALS = {
+    os.getenv("ROOT_USER"): os.getenv("ROOT_PASS"),
+    os.getenv("GUEST_PASS"): os.getenv("GUEST_PASS")
+}
+
+@auth.verify_password
+def verify(username, password):
+    if not (username and password):
+        return False
+    if request.path == '/':
+        if username in VALID_CREDENTIALS and password == VALID_CREDENTIALS[username]:
+            return True
+        return False
+    else:
+        return True
+
 @app.route('/', methods=['GET'])
 def main_LB_management():
 
